55af29f1a597a3ec135d3d7a77933095a8725d64
Georgia Power <131956221+gp-rgb@users.noreply.github.com>
2024-02-19 15:20:37 +1000

    fix client

--- view_splatt.sh
+++ view_splatt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source config.txt
-
+lsof -ti:${PORT_NUMBER} | xargs kill -9
 ssh -L "${PORT_NUMBER}:0.0.0.0:${PORT_NUMBER}" "${SSH_ALIAS}" -N &
 sleep 30
-firefox http://[::]:${PORT_NUMBER}/
+#python -m http.client ${PORT_NUMBER}
+open -a firefox -g http://0.0.0.0:${PORT_NUMBER}/
 wait
