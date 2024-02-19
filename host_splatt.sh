ebfec49674f678db26325a6788730d40ed5db15b
gp-rgb <georgia@powertp.com>
2024-02-19 05:20:11 +0000

    fix host

--- host_splatt.sh
+++ host_splatt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+source config.txt
 cd /home/ubuntu/georgia/splatt/
 git clone https://github.com/antimatter15/splat.git
 cd splat
 pip install plyfile argparse  
-#pointcloud=/home/ubuntu/georgia/data/orchid_splatt/orchid_splatt_model/point_cloud/iteration_5000/point_cloud.ply
-#python convert.py ${pointcloud}
-python /home/ubuntu/georgia/ingp_scripts/server_url.py 8000 ./output.splat
+python convert.py ${MODEL_PATH}
+python -m http.server ${PORT_NUMBER}
